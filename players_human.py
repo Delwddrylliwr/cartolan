9e2a81737400337f1ccc4c8cdf0f6a5704c2758b
delwddrylliwr <delwddrylliwr@gmail.com>
2022-07-31 16:27:29 +0100

    Added updated Card art. Modified card visualisation so that selected cards are shown mid-screen rather than on top of the rest of the stack.

--- players_human.py
+++ players_human.py
@@ -183,20 +183,7 @@ class PlayerHuman(Player):
         #make sure that tiles and token positions are up to date
         game_vis.draw_play_area()
         game_vis.draw_routes()
-        #Draw the left menu items
-        game_vis.draw_scores()
-        if isinstance(adventurer, AdventurerAdvanced):
-            game_vis.draw_cards()
-        #Draw the right menu items
-        game_vis.draw_move_count()
-        game_vis.draw_toggle_menu(self.auto_actions)
-        if isinstance(adventurer, AdventurerRegular):
-            adventurer.match_chest_directions()
-            game_vis.draw_chest_tiles()
-        game_vis.draw_tile_piles()
-        game_vis.draw_discard_pile()
-        game_vis.draw_undo_button()
-        
+
         game_vis.draw_move_options(moves)
         game_vis.draw_tokens() #draw them on top of highlights
             
@@ -210,7 +197,21 @@ class PlayerHuman(Player):
             prompt += " Or select a route to follow."
         game_vis.current_player_name = adventurer.player.name
         game_vis.give_prompt(prompt)
-        
+
+        # Draw the left menu items
+        game_vis.draw_scores()
+        if isinstance(adventurer, AdventurerAdvanced):
+            game_vis.draw_cards()
+        # Draw the right menu items
+        game_vis.draw_move_count()
+        game_vis.draw_toggle_menu(self.auto_actions)
+        if isinstance(adventurer, AdventurerRegular):
+            adventurer.match_chest_directions()
+            game_vis.draw_chest_tiles()
+        game_vis.draw_tile_piles()
+        game_vis.draw_discard_pile()
+        game_vis.draw_undo_button()
+
         #Carry out the player's chosen move
         player_input = {"Nothing":"Nothing"}
         while player_input.get("Nothing") is not None:
@@ -247,11 +248,15 @@ class PlayerHuman(Player):
                 #Redraw everything in case the adventurer of focus was changed
                 game_vis.draw_play_area()
                 game_vis.draw_routes()
-                #Draw the left menu items
+
+                game_vis.draw_move_options(moves)
+                game_vis.draw_tokens() #draw them on top of highlights
+                game_vis.give_prompt(prompt)
+                # Draw the left menu items
                 game_vis.draw_scores()
                 if isinstance(adventurer, AdventurerAdvanced):
                     game_vis.draw_cards()
-                #Draw the right menu items
+                # Draw the right menu items
                 game_vis.draw_move_count()
                 game_vis.draw_toggle_menu(self.auto_actions)
                 if isinstance(adventurer, AdventurerRegular):
@@ -260,15 +265,11 @@ class PlayerHuman(Player):
                 game_vis.draw_tile_piles()
                 game_vis.draw_discard_pile()
                 game_vis.draw_undo_button()
-                
-                game_vis.draw_move_options(moves)
-                game_vis.draw_tokens() #draw them on top of highlights
-                game_vis.give_prompt(prompt)
                 #Seek input again
                 player_input = game_vis.get_input_coords(adventurer)
                 print("Player's input:")
                 print(player_input)
-        
+
         if player_input.get("move") is not None:
             move_coords = player_input["move"]
 #            print(self.name+" player chose valid coordinates to move to.")
@@ -406,20 +407,7 @@ class PlayerHuman(Player):
         #make sure that tiles and token positions are up to date
         game_vis.draw_play_area()
         game_vis.draw_routes()
-        #Draw the left menu items
-        game_vis.draw_scores()
-        if isinstance(adventurer, AdventurerAdvanced):
-            game_vis.draw_cards()
-        #Draw the right menu items
-        game_vis.draw_move_count()
-        game_vis.draw_toggle_menu(self.auto_actions)
-        if isinstance(adventurer, AdventurerRegular):
-            adventurer.match_chest_directions()
-            game_vis.draw_chest_tiles()
-        game_vis.draw_tile_piles()
-        game_vis.draw_discard_pile()
-        game_vis.draw_undo_button()
-        
+
         print("Adding movement options that will be available next move, to allow skipping of actions")
         if not isinstance(adventurer.current_tile, CityTile): #If this is a buying action from the city then the turn is about to end, even with spare moves
             moves, move_map = self.establish_moves(adventurer)
@@ -442,6 +430,20 @@ class PlayerHuman(Player):
 #            game_vis.clear_prompt()
         game_vis.current_player_name = adventurer.player.name
         game_vis.give_prompt(prompt)
+
+        # Draw the left menu items
+        game_vis.draw_scores()
+        if isinstance(adventurer, AdventurerAdvanced):
+            game_vis.draw_cards()
+        # Draw the right menu items
+        game_vis.draw_move_count()
+        game_vis.draw_toggle_menu(self.auto_actions)
+        if isinstance(adventurer, AdventurerRegular):
+            adventurer.match_chest_directions()
+            game_vis.draw_chest_tiles()
+        game_vis.draw_tile_piles()
+        game_vis.draw_discard_pile()
+        game_vis.draw_undo_button()
         
         action_location = None
 #        player_input = None
@@ -462,22 +464,23 @@ class PlayerHuman(Player):
             #make sure that tiles and token positions are up to date
             game_vis.draw_play_area()
             game_vis.draw_routes()
-            #Draw the left menu items
-            game_vis.draw_scores()
-            if isinstance(adventurer, AdventurerAdvanced):
-                game_vis.draw_cards()
-            #Draw the right menu items
-            game_vis.draw_move_count()
-            game_vis.draw_toggle_menu(self.auto_actions)
-            if isinstance(adventurer, AdventurerRegular):
-                adventurer.match_chest_directions()
-                game_vis.draw_chest_tiles()
             game_vis.draw_tile_piles()
             game_vis.draw_discard_pile()
             game_vis.draw_undo_button()
             game_vis.draw_move_options(actions)
             game_vis.draw_tokens() #draw tokens on top of highlights
             game_vis.give_prompt(prompt)
+            # Draw the left menu items
+            game_vis.draw_scores()
+            if isinstance(adventurer, AdventurerAdvanced):
+                game_vis.draw_cards()
+            # Draw the right menu items
+            game_vis.draw_move_count()
+            game_vis.draw_toggle_menu(self.auto_actions)
+            if isinstance(adventurer, AdventurerRegular):
+                adventurer.match_chest_directions()
+                game_vis.draw_chest_tiles()
+
             #Seek input again
             player_input = game_vis.get_input_coords(adventurer)
         if player_input.get(action_type) is not None:
